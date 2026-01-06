0ec5af6747a80b6ecc4f38f263fcf5314ddae0b8
PandukaWijesinghee <pandukawijesinghe@gmail.com>
2026-01-06 15:53:45 +0530

    Update nav_booking_screen.dart

--- lib/screens/bus_booking/nav_booking_screen.dart
+++ lib/screens/bus_booking/nav_booking_screen.dart
@@ -22,7 +22,7 @@ class _BookingScreenState extends State<BookingScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primary,
+      backgroundColor: AppColors.background,
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
